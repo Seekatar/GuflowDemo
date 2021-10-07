b28cecf4fa0b317bb7d539a249686f808407cbe9
Jim Wallace <jim_wallace@aisreview.com>
2021-10-07 12:47:32 -0400

    broken signal

--- src/CCC.CAS.Workflow3Service/Workflows/PpoWorkflowSignaled.cs
+++ src/CCC.CAS.Workflow3Service/Workflows/PpoWorkflowSignaled.cs
@@ -1,5 +1,7 @@
-﻿using CCC.CAS.Workflow3Service.Activities;
+﻿using CCC.CAS.Workflow3Messages.Messages;
+using CCC.CAS.Workflow3Service.Activities;
 using Guflow.Decider;
+using System.Linq;
 
 namespace CCC.CAS.Workflow3Service.Workflows
 {
@@ -12,19 +14,38 @@ namespace CCC.CAS.Workflow3Service.Workflows
         public PpoWorkflowSignaled()
         {
             ScheduleActivity<PpoProcessorA>()
-                .OnCompletion(e => e.WaitForSignal("Signal-PpoProcessorA"));
+                .OnCompletion(e => e.WaitForSignal("Signal-PpoProcessorA"))
+                ;
 
             ScheduleActivity<PpoProcessorB>()
-                .AfterActivity<PpoProcessorA>()
+//                .When(_ => Signal("Signal-PpoProcessorA").IsTriggered())
+                .OnCompletion(e => e.WaitForSignal("Signal-PpoProcessorB"))
+                ;
+
+            //ScheduleActivity<PpoProcessorC>()
+            //    .AfterActivity<PpoProcessorB>()
+            //    .OnCompletion(e => e.WaitForSignal("Signal-PpoProcessorC"));
+
+            //// added this because after C waits forever, even though signaled
+            //ScheduleActivity<PpoEnd>()
+            //    .When(a => a.ParentActivity().Result<StartPpoResponse>().Started)
+            //    .AfterActivity<PpoProcessorC>();
+        }
+
+        [SignalEvent(Name = "Signal-PpoProcessorQ")]
+        public WorkflowAction OnSignal(WorkflowSignaledEvent e)
+        {
+            System.Console.WriteLine(e);
+
+            // does nothing
+            ScheduleActivity<PpoProcessorB>()
                 .OnCompletion(e => e.WaitForSignal("Signal-PpoProcessorB"));
 
-            ScheduleActivity<PpoProcessorC>()
-                .AfterActivity<PpoProcessorB>()
-                .OnCompletion(e => e.WaitForSignal("Signal-PpoProcessorC"));
+            // doesn't work since "PpoProcessorB" isn't running. We need to remove it from the graph
+            //return CancelRequest.For(WorkflowItems.Where(i => (i as IActivityItem)?.Name == "PpoProcessorB"));
 
-            // added this because after C waits forever, even though signaled
-            ScheduleActivity<PpoEnd>()
-                .AfterActivity<PpoProcessorC>();
+            return Ignore; 
         }
+
     }
 }
